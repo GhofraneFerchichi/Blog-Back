8f00c6a3581e2792aeb1aeca40d65ec7514cfe23
MARYE1998 <maryem.tayeb@esprit.tn>
2023-04-23 00:34:36 +0100

    Crud

---
+++ src/main/java/com/roky/thunderspi/entities/Comment.java
@@ -0,0 +1,23 @@
+package com.roky.thunderspi.entities;
+
+import lombok.*;
+import lombok.experimental.FieldDefaults;
+
+import javax.persistence.*;
+
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@FieldDefaults(level = AccessLevel.PRIVATE)
+@Entity
+@Table(name = "commentblog")
+public class Comment  {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    Long id;
+    String title;
+    String content;
+    String picture;
+
+}
